b44eee06494837dc3b51f777b963a459c601d1b8
1160300510 <379744795@qq.com>
2020-11-08 13:47:31 +0800

    122.买卖股票的最佳时机II

---
+++ src/Easy/maxProfit.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class maxProfit {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]){
+                ans += prices[i]-prices[i-1];
+            }
+        }
+        return ans;
+    }
+}
