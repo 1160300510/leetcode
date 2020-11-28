6692bfd08bc8807bcf24a8c065e2e73a228bcb5e
1160300510 <379744795@qq.com>
2020-11-28 15:49:03 +0800

    493.翻转对

---
+++ src/Hard/reversePairs.java
@@ -0,0 +1,31 @@
+package Hard;
+
+import java.util.*;
+
+public class reversePairs {
+    public int reversePairs(int[] nums) {
+        Set<Long> allNumbers = new TreeSet<Long>();
+        for (int x : nums) {
+            allNumbers.add((long) x);
+            allNumbers.add((long) x * 2);
+        }
+        // 利用哈希表进行离散化
+        Map<Long, Integer> values = new HashMap<Long, Integer>();
+        int idx = 0;
+        for (long x : allNumbers) {
+            values.put(x, idx);
+            idx++;
+        }
+
+        int ret = 0;
+        // 树状数组
+        BIT bit = new BIT(values.size());
+        for (int i = 0; i < nums.length; i++) {
+            int left = values.get((long) nums[i] * 2), right = values.size() - 1;
+            ret += bit.query(right + 1) - bit.query(left + 1);
+            bit.update(values.get((long) nums[i]) + 1, 1);
+        }
+        return ret;
+    }
+}
+
