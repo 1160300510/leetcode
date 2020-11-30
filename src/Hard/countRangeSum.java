5959c5bdc71b61c937d6f611873cf8a3cfff4310
1160300510 <379744795@qq.com>
2020-11-30 22:17:41 +0800

    327.区间和的个数

--- src/Hard/countRangeSum.java
+++ src/Hard/countRangeSum.java
@@ -36,7 +36,60 @@ public class countRangeSum {
             bit.update(values.get(preSum[i]) + 1, 1);
         }
         return ret;
+    }
 
+    public int countRangeSum2(int[] nums, int lower, int upper){
+        long s = 0;
+        long[] sum = new long[nums.length+1];
+        for(int i=0; i<nums.length; i++){
+            s += nums[i];
+            sum[i+1] = s;
+        }
+        return countRangeSumRecursive(sum, lower, upper, 0, sum.length-1);
+    }
+
+    public int countRangeSumRecursive(long[] sum, int lower, int upper, int left, int right){
+        if(left == right){
+            return 0;
+        }
+        int mid = (left+right)/2;
+        int n1 = countRangeSumRecursive(sum, lower, upper, left, mid);
+        int n2 = countRangeSumRecursive(sum, lower, upper, mid+1, right);
+        int ret = n1+n2;
+
+        int l = mid+1, r=mid+1;
+        int i = left;
+        while (i <= mid){
+            while (l<=right && (sum[l]-sum[i])<lower){
+                l++;
+            }
+            while (r<=right && (sum[r]-sum[i])<=upper){
+                r++;
+            }
+            ret += r-l;
+            i++;
+        }
+        //合并两个排序数组
+        int[] sorted = new int[right-left+1];
+        int p1 = left, p2 = mid+1;
+        int p = 0;
+        while (p1<=mid || p2<=right){
+            if(p1 > mid){
+                sorted[p++] = (int) sum[p2++];
+            }else if(p2 > right){
+                sorted[p++] = (int) sum[p1++];
+            }else {
+                if(sum[p1]<sum[p2]){
+                    sorted[p++] = (int)sum[p1++];
+                }else {
+                    sorted[p++] = (int)sum[p2++];
+                }
+            }
+        }
+        for(int k=0; k<sorted.length; k++){
+            sum[left+k] = sorted[k];
+        }
+        return ret;
     }
 }
 
