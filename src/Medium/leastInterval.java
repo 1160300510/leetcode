319909c67b36f71448d8871fee824e137bd021e5
1160300510 <379744795@qq.com>
2020-12-05 11:22:19 +0800

    621.任务调度器

---
+++ src/Medium/leastInterval.java
@@ -0,0 +1,29 @@
+package Medium;
+
+import java.util.*;
+
+public class leastInterval {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> freq = new HashMap<Character, Integer>();
+        // 最多的执行次数
+        int maxExec = 0;
+        for (char ch : tasks) {
+            int exec = freq.getOrDefault(ch, 0) + 1;
+            freq.put(ch, exec);
+            maxExec = Math.max(maxExec, exec);
+        }
+
+        // 具有最多执行次数的任务数量
+        int maxCount = 0;
+        Set<Map.Entry<Character, Integer>> entrySet = freq.entrySet();
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            int value = entry.getValue();
+            if (value == maxExec) {
+                ++maxCount;
+            }
+        }
+
+        return Math.max((maxExec - 1) * (n + 1) + maxCount, tasks.length);
+
+    }
+}
