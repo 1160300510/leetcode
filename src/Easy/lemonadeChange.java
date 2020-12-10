34f90acc821e7e5a3ed0f9af35517a4c4a64bb62
1160300510 <379744795@qq.com>
2020-12-10 10:15:37 +0800

    860. 柠檬水找零

---
+++ src/Easy/lemonadeChange.java
@@ -0,0 +1,31 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class lemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0, ten=0;
+        for(int bill : bills){
+            if(bill == 5){
+                five++;
+            }else if(bill == 10){
+                if(five == 0){
+                    return false;
+                }
+                five--;
+                ten++;
+            }else if(bill == 20){
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }else if(five >= 3){
+                    five -= 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
