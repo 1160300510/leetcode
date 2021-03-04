0dfe83540f5cfec357f3020e32f493c578c961f0
fundamental21 <379744795@qq.com>
2021-03-04 10:26:17 +0800

    338. 比特位计数

---
+++ src/Medium/countBits.java
@@ -0,0 +1,11 @@
+package Medium;
+
+public class countBits {
+    public int[] countBits(int num) {
+        int[] dp = new int[num+1];
+        for(int i=0; i<=num; ++i){
+            dp[i] = dp[i>>1] + (i&1);
+        }
+        return dp;
+    }
+}
