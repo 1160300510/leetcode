52cdfd0b8f9cd0fad1357e37243c38547c63a8e8
1160300510 <379744795@qq.com>
2021-03-16 20:21:59 +0800

    4. 寻找两个正序数组的中位数

---
+++ src/剑指offer/MedianofTwoSortedArrays.java
@@ -0,0 +1,44 @@
+package 剑指offer;
+
+public class MedianofTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int totallen = len1+len2;
+        if(totallen % 2 == 1){
+            int index = totallen/2;
+            return getKthElement(nums1, nums2, index+1);
+        }else{
+            int index1 = totallen/2-1, index2 = totallen/2;
+            return (getKthElement(nums1,nums2,index1+1)+getKthElement(nums1,nums2,index2+1))/2.0;
+        }
+    }
+    public double getKthElement(int[] nums1, int[] nums2, int k){
+        int index1 = 0, index2 = 0;
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        while (true){
+            if(index1 >= len1){
+                return nums2[index2+k-1];
+            }
+            if(index2 >= len2){
+                return nums1[index1+k-1];
+            }
+            if(k == 1){
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+            int pivot = k/2;
+            int newIndex1 = Math.min(len1, index1+pivot)-1;
+            int newIndex2 = Math.min(len2, index2+pivot)-1;
+            int a = nums1[newIndex1];
+            int b = nums2[newIndex2];
+            if(nums1[a] <= nums2[b]){
+                k -= (newIndex1-index1+1);
+                index1 = newIndex1+1;
+            }else{
+                k -= (newIndex2-index2+1);
+                index2 = newIndex2+1;
+            }
+        }
+    }
+}
