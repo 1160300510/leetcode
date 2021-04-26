faca30b8af4f6501434449eb11e0b7c1038ee99b
1160300510 <379744795@qq.com>
2021-04-26 09:34:07 +0800

    1011. 在 D 天内送达包裹的能力

---
+++ src/Medium/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,33 @@
+package Medium;
+
+public class CapacityToShipPackagesWithinDDays {
+    public int shipWithinDays(int[] weights, int D) {
+        int l = 0, r = 0;
+        for(int i=0; i<weights.length; i++){
+            l = Math.max(l, weights[i]);
+            r += weights[i];
+        }
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(check(weights, D, mid)){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    public boolean check(int[] weights, int D, int m){
+        int sum = 0, cnt = 1;
+        for(int weight : weights){
+            if(sum + weight <= m){
+                sum += weight;
+            }else {
+                sum = weight;
+                cnt++;
+            }
+        }
+        return D>=cnt;
+    }
+}
